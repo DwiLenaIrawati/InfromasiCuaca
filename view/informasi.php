e7a6204741000e50dcb1a5d766cd7c1b1975d935
Dwi Lena Irawati <86524394+DwiLenaIrawati@users.noreply.github.com>
2021-12-14 00:33:51 -0800

    Add files via upload

---
+++ view/informasi.php
@@ -0,0 +1,164 @@
+<div id="informasi">
+
+    <div class="w3-container" id="subtitle">
+       
+		<a id="kota" style="font-size:x-large;text-decoration:none">Kota</a>
+        <a id="select">
+			<select name="test">
+				<option value="1">Select 1</option>
+				<option value="2">Select 2</option>
+				<option value="3">Select 3</option>
+				<option value="4">Select 4</option>
+				<option value="5">Select 5</option>
+           	</select>
+		</a>
+        <a class="date"><input type="date"> </a>
+        <a >SD</a>
+        <a class="date"><input type="date"> </a>    
+        
+    </div>
+    <div id="hasil" style="color: #629AAE;">
+        <div class="w3-container">
+            
+			<div class="w3-row w3-container">
+				<div class="w3-row">
+					<div class="w3-col m2 w3-center">
+						<div id="date">
+							<p id="valueDate">12-3-2021</p>
+						</div>
+						<div id="suhu" style="font-size:x-large;">
+							<p id="valueSuhu">27C</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<br>
+						<div id="waktu" style="font-size:large;color: #629AAE;">
+							<p>Pagi</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="angin">
+							<a><i class="fas fa-wind fa-2x"></i></a>
+							<p>Kecepatan Angin</p>
+							<p id="valueAngin">Value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="hujan">
+							<a><i class="fas fa-umbrella fa-2x"></i></a>
+							<p>Hujan</p>
+							<p id="valueHujan">value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="curah">
+							<a><i class="fas fa-cloud-rain fa-2x"></i></a>
+							<p>Curah Hujan</p>
+							<p id="valueCurah">value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center" >
+						<div id="kelembapan">
+							<a><i class="fas fa-tint fa-2x"></i></a>
+							<p>Kelembapan</p>
+							<p id="valueKelembapan">value</p>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="w3-row">
+					<div class="w3-col m2 w3-center">
+						<div id="date">
+							<p id="valueDate">12-3-2021</p>
+						</div>
+						<div id="suhu" style="font-size:x-large;">
+							<p id="valueSuhu">27C</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<br>
+						<div id="waktu" style="font-size:large;color: #629AAE;">
+							<p>Pagi</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="angin">
+							<a><i class="fas fa-wind fa-2x"></i></a>
+							<p>Kecepatan Angin</p>
+							<p id="valueAngin">Value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="hujan">
+							<a><i class="fas fa-umbrella fa-2x"></i></a>
+							<p>Hujan</p>
+							<p id="valueHujan">value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="curah">
+							<a><i class="fas fa-cloud-rain fa-2x"></i></a>
+							<p>Curah Hujan</p>
+							<p id="valueCurah">value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center" >
+						<div id="kelembapan">
+							<a><i class="fas fa-tint fa-2x"></i></a>
+							<p>Kelembapan</p>
+							<p id="valueKelembapan">value</p>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="w3-row">
+					<div class="w3-col m2 w3-center">
+						<div id="date">
+							<p id="valueDate">12-3-2021</p>
+						</div>
+						<div id="suhu" style="font-size:x-large;">
+							<p id="valueSuhu">27C</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<br>
+						<div id="waktu" style="font-size:large;color: #629AAE;">
+							<p>Pagi</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="angin">
+							<a><i class="fas fa-wind fa-2x"></i></a>
+							<p>Kecepatan Angin</p>
+							<p id="valueAngin">Value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="hujan">
+							<a><i class="fas fa-umbrella fa-2x"></i></a>
+							<p>Hujan</p>
+							<p id="valueHujan">value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center">
+						<div id="curah">
+							<a><i class="fas fa-cloud-rain fa-2x"></i></a>
+							<p>Curah Hujan</p>
+							<p id="valueCurah">value</p>
+						</div>
+					</div>
+					<div class="w3-col m2 w3-center" >
+						<div id="kelembapan">
+							<a><i class="fas fa-tint fa-2x"></i></a>
+							<p>Kelembapan</p>
+							<p id="valueKelembapan">value</p>
+						</div>
+					</div>
+				</div>
+            </div>
+        </div>
+    </div>
+	<button id="grafik"class="w3-button w3-flat-belize-hole  w3-round" style="margin-left: 1200px;">Grafik</button>
+</div>
+
+
